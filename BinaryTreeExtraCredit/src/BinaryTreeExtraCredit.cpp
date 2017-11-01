50b1afb967bec2e544d6cd828e81116d087fe8ff
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-11-01 08:50:31 -0700

    Finished assignment
    No logic, just call the functions that the teacher had.

--- BinaryTreeExtraCredit/src/BinaryTreeExtraCredit.cpp
+++ BinaryTreeExtraCredit/src/BinaryTreeExtraCredit.cpp
@@ -7,9 +7,39 @@
 //============================================================================
 
 #include <iostream>
+#include "BinarySearchTree.h"
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	BinarySearchTree<int> binaryTree;
+
+	int input;
+	cout << "Enter a number(0 to exit):";
+	cin >> input;
+
+	while (input != 0)
+	{
+		binaryTree.insert(input);
+
+
+		cout << "Enter a number(0 to exit):";
+		cin >> input;
+	}
+
+	cout << "inorder traversal: ";
+	binaryTree.inOrder();
+	cout << endl;
+
+	cout << "preorder traversal: ";
+	binaryTree.preOrder();
+	cout << endl;
+
+	cout << "postorder traversal: ";
+	binaryTree.postOrder();
+	cout << endl;
+
+	cout << "Node count: " << binaryTree.nodeCount() << endl;
+	cout << "Leaves count: " << binaryTree.leaveCount() << endl;
+	cout << "Height: " <<  binaryTree.height() << endl;
 	return 0;
 }
